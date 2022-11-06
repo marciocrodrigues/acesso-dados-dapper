e533d9a89a8a1674cd9fbebe86323442e4a19064
marciocostarodrigues <marciodeath@hotmail.com>
2022-11-06 12:47:54 -0300

    Alteração do codigo

--- Program.cs
+++ Program.cs
@@ -5,6 +5,7 @@ using Dapper;
 using Introducao.Models;
 using System.Linq;
 using System.Reflection.PortableExecutable;
+using System.Collections.Generic;
 
 namespace Introducao
 {
@@ -19,55 +20,24 @@ namespace Introducao
             // ExecutarSql(connectionString);
 
             // Utilizando dapper
-            ExecuteSqlViaDapper(connectionString, false);
+            ExecuteSqlViaDapper(connectionString, false, true);
 
             Console.ReadKey();
         }
 
-        static void ExecuteSqlViaDapper(string connectionString, bool insert = false)
+        static void ExecuteSqlViaDapper(string connectionString, bool insert = false, bool consulta = false)
         {
-            var category = new Category();
-            string insertSql = string.Empty;
-
             if (insert)
-            {
-                category.Id = Guid.NewGuid();
-                category.Title = "Amazon AWS";
-                category.Url = "amazon";
-                category.Description = "Categoria destinada a serviços do AWS";
-                category.Order = 8;
-                category.Summary = "AWS Cloud";
-                category.Featured = false;
+                ExecuteInsertCategory(connectionString);
 
-                insertSql = @"
-                INSERT INTO
-                    [Category]
-                VALUES (
-                    @Id,
-                    @Title,
-                    @Url,
-                    @Summary,
-                    @Order,
-                    @Description,
-                    @Featured)";
-            }
-            
+            if (consulta)
+                ExecuteListCategory(connectionString);
+        }
+
+        static void ExecuteListCategory(string connectionString)
+        {
             using (var connection = new SqlConnection(connectionString))
             {
-                if (insert)
-                {
-                    connection.Execute(insertSql, new
-                    {
-                        Id = category.Id,
-                        Title = category.Title,
-                        Url = category.Url,
-                        Summary = category.Summary,
-                        Order = category.Order,
-                        Description = category.Description,
-                        Featured = category.Featured
-                    });
-                }
-
                 // Dapper é uma extensão para executar comandos no banco
                 var categories = connection.Query<Category>("SELECT [Id], [Title], [Url], [Summary], [Order], [Description], [Featured] FROM [Category]");
 
@@ -78,6 +48,46 @@ namespace Introducao
                         Console.WriteLine($"{item.Id} - {item.Title} - {item.Order} - {item.Summary}");
                     }
                 }
+            }
+        }
+
+        static void ExecuteInsertCategory(string connectionString)
+        {
+            var category = new Category();
+            
+            category.Id = Guid.NewGuid();
+            category.Title = "Amazon AWS";
+            category.Url = "amazon";
+            category.Description = "Categoria destinada a serviços do AWS";
+            category.Order = 8;
+            category.Summary = "AWS Cloud";
+            category.Featured = false;
+
+            var insertSql = @"
+                INSERT INTO
+                    [Category]
+                VALUES (
+                    @Id,
+                    @Title,
+                    @Url,
+                    @Summary,
+                    @Order,
+                    @Description,
+                    @Featured)";
+
+            using (var connection = new SqlConnection(connectionString))
+            {
+                connection.Execute(insertSql, new
+                {
+                    Id = category.Id,
+                    Title = category.Title,
+                    Url = category.Url,
+                    Summary = category.Summary,
+                    Order = category.Order,
+                    Description = category.Description,
+                    Featured = category.Featured
+                });
+
             }
         }
 
